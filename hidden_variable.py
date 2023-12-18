edf4f5e8134135829fd5d96eb2a6875fe5851e3c
Saurav Sharma <73769851+saurav-2k01@users.noreply.github.com>
2023-12-18 15:41:39 +0530

    Add files via upload

---
+++ hidden_variable.py
@@ -0,0 +1,11 @@
+import datetime
+
+API_KEY = "6873085439:AAETGsFNiydy0irJHU2fqqbHeDruGMEEEw0"
+
+URL = "https://13.201.79.147:8443"
+
+_update_ = {
+    "update":"webhook integration-1",
+    "description":"webhook integration was done for scaling the bot for large number of users.",
+}
+
